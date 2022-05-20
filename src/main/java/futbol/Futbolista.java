7443b2bef5f6ec0767ae2f4f37e5ef1beaf31e64
Julián Orozco V <jorozcova@unal.edu.co>
2022-05-20 18:45:11 -0500

    intento 1

---
+++ src/main/java/futbol/Futbolista.java
@@ -0,0 +1,47 @@
+package futbol;
+
+public abstract class Futbolista implements Comparable<Futbolista> {
+	private String nombre;
+	private int edad;
+	private String posicion;
+	
+	public Futbolista() {
+		this("Maradona",30,"delantero");
+	}
+	
+    public Futbolista(String nombre, int edad, String posicion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.posicion = posicion;
+    }
+    
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getPosicion() {
+        return this.posicion;
+    }
+    
+    public String toString() {
+    	return "El futbolista "+ this.getNombre()+" tiene "+this.getEdad()+ ", yjuega de "+this.getPosicion();
+    }
+    
+    public boolean equals(Futbolista f) {
+    	return this.equals(f);
+    }
+    
+    public abstract boolean jugarConLasManos();
+}
---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,26 @@
+package futbol;
+
+public class Portero extends Futbolista{
+	public short golesRecibidos;
+	public byte dorsal;
+	
+	public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+	      super(nombre, edad, "Portero");
+	      this.golesRecibidos = golesRecibidos;
+	      this.dorsal = dorsal;
+	  }
+	
+	public String toString() {
+    	return "El futbolista "+ this.getNombre()+" tiene "+this.getEdad()+ ", yjuega de "+this.getPosicion() + "con el dorsal "
+    			+this.dorsal+". Le han marcado "+this.golesRecibidos;
+    }
+
+	public int compareTo(Futbolista o) {
+		return Math.abs(this.golesRecibidos - ((Portero)o).golesRecibidos);
+	}
+
+	public boolean jugarConLasManos() {
+		return true;
+	}
+	
+}
